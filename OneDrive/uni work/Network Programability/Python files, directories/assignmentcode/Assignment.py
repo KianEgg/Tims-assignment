c2f55d299de92cf6fcd5f09c3f1c294879d3d785
KianEgg <2204995@student.uwtsd.ac.uk>
2024-01-19 17:01:35 +0000

    third push

--- OneDrive/uni work/Network Programability/Python files, directories/assignmentcode/Assignment.py
+++ OneDrive/uni work/Network Programability/Python files, directories/assignmentcode/Assignment.py
@@ -2,16 +2,6 @@ import json
 import requests
 
 
-#Get json data from source 
-response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m")
-data = response.json()       
-
-#Fetches and outputs all collected data + message confirming data fetch was successful or not
-if response.status_code == 200:
-    print("sucessfully fetched the data")
-    print(data)
-else:
-    print(f"Hello person, there's a {response.status_code} error with your request")
 
 
 
