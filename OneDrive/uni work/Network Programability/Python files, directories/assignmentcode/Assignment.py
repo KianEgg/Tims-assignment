c9a085c31f941b906cb292efe9851943ee4547d1
KianEgg <2204995@student.uwtsd.ac.uk>
2024-01-19 16:58:20 +0000

    second push

--- OneDrive/uni work/Network Programability/Python files, directories/assignmentcode/Assignment.py
+++ OneDrive/uni work/Network Programability/Python files, directories/assignmentcode/Assignment.py
@@ -2,15 +2,11 @@ import json
 import requests
 
 
-
 #Get json data from source 
 response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m")
 data = response.json()       
 
-
-
-
-#Fetches and outputs all collected data + message confirming data fetch was successful or not 
+#Fetches and outputs all collected data + message confirming data fetch was successful or not
 if response.status_code == 200:
     print("sucessfully fetched the data")
     print(data)
@@ -18,21 +14,28 @@ else:
     print(f"Hello person, there's a {response.status_code} error with your request")
 
 
+
 #allows users to input there desired longitude and latitude 
 longitude = str(input("please enter your desired longitude:"))
 latitude = str(input("please enter your desired latitude:"))
+
+     
 
 
 #changes the longitude and latitude in api url to users inputted values 
 enter_data= requests.get(
     f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m"
 )
-  
+print(enter_data.status_code) 
+
+data1 = enter_data.json()
+print(data1)
+
+def extract_key_value(data1):
+    #data = json.loads(data1)
+    units = data1 ['current_units'] ['temperature_2m']
+    value= data1 ['current'] ['temperature_2m']
+    print("the temperature at your location is", value ,units)
 
 
-
-
-def extract_key_value(data, timezone):
-    data = json.loads(data)
-    value = data.get(timezone)
-    print(value)
+extract_key_value(data1)
